53be03cd42e655977a18068d133eb8ed46bfaafc
karanrohit868@gmail.com <karanrohit868@gmail.com>
2026-04-22 23:45:29 +0530

    final update: auth + sidebar + routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -8,6 +7,7 @@ import Visitors from "./pages/Visitors";
 import Complaints from "./pages/Complaints";
 import Announcements from "./pages/Announcements";
 import ForgotPassword from "./pages/ForgotPassword";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -17,18 +17,33 @@ function App() {
       <BrowserRouter>
         <Routes>
 
-          {/* ✅ ADD THIS */}
           <Route path="/login" element={<Login />} />
 
-          {/* ✅ KEEP THIS (optional but good) */}
-          <Route path="/" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              localStorage.getItem("token") ? <Dashboard /> : <Login />
+            }
+          />
 
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/visitors" element={<Visitors />} />
-          <Route path="/complaints" element={<Complaints />} />
-          <Route path="/announcements" element={<Announcements />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          <Route path="/dashboard" element={
+            <ProtectedRoute><Dashboard /></ProtectedRoute>
+          } />
+
+          <Route path="/visitors" element={
+            <ProtectedRoute><Visitors /></ProtectedRoute>
+          } />
+
+          <Route path="/complaints" element={
+            <ProtectedRoute><Complaints /></ProtectedRoute>
+          } />
+
+          <Route path="/announcements" element={
+            <ProtectedRoute><Announcements /></ProtectedRoute>
+          } />
 
         </Routes>
       </BrowserRouter>
